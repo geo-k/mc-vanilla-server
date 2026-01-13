c00779f2c32e370c713d7c3e931e11f6db0405e7
geo-k <91480810+geo-k@users.noreply.github.com>
2026-01-13 17:36:07 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/update.sh -O update.sh
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/update.sh -O update.sh
 chmod +x update.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/update-server.sh -O update-server.sh
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/update-server.sh -O update-server.sh
 chmod +x update-server.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/start.sh -O start.sh
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/start.sh -O start.sh
 chmod +x start.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/stop.sh -O stop.sh
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/stop.sh -O stop.sh
 chmod +x stop.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/logs.sh -O logs.sh
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/logs.sh -O logs.sh
 chmod +x stop.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/docker-compose.yml -O docker-compose.yml
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/docker-compose.yml -O docker-compose.yml
 
 ./update-server.sh
