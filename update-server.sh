12666c641c4e906b4996f0ccf4e680344571d08b
geo-k <91480810+geo-k@users.noreply.github.com>
2026-01-13 17:37:49 -0500

    Update update-server.sh

--- update-server.sh
+++ update-server.sh
@@ -6,14 +6,10 @@ docker pull itzg/minecraft-server
 
 rm -Rf ./data/mods/
 mkdir -p ./data/mods/
-mkdir -p ./data/world-2025/serverconfig/
+mkdir -p ./data/world-2026/serverconfig/
 rm -Rf ./downloads/
 mkdir -p ./downloads/
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/server-config/mantle-server.toml -O ./data/world-2025/serverconfig/mantle-server.toml
-
-
-# Download mods that can't be auo loaded with curseforge due to licencing 
-wget https://cdn.modrinth.com/data/gu7yAYhd/versions/GYnSPc0b/cc-tweaked-1.20.1-forge-1.115.1.jar -O downloads/cc-tweaked-1.20.1-forge-1.115.1.jar
+wget https://raw.githubusercontent.com/geo-k/mc-vanilla-server/main/server-config/mantle-server.toml -O ./data/world-2026/serverconfig/mantle-server.toml
 
 ./start.sh
